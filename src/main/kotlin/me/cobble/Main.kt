9097e3c6138d07f0f4ae4af630b1a26aa6590734
boop-the-dog <aidenhperry@gmail.com>
2022-05-05 22:10:30 -0400

    hopelessness

--- src/main/kotlin/me/cobble/Main.kt
+++ src/main/kotlin/me/cobble/Main.kt
@@ -8,8 +8,6 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 
-const val prefix: String = "$"
-
 fun main() {
     println("Logging in...")
     val client: JDA = JDABuilder.createDefault(System.getenv("BOT_TOKEN")).build()
--- src/main/kotlin/me/cobble/commands/InviteCommand.kt
+++ src/main/kotlin/me/cobble/commands/InviteCommand.kt
@@ -2,10 +2,11 @@ package me.cobble.commands
 
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class InviteCommand(api: JDA) : ListenerAdapter() {
+class InviteCommand(private val api: JDA) : ListenerAdapter() {
 
     init {
         api.addEventListener(this)
@@ -18,7 +19,7 @@ class InviteCommand(api: JDA) : ListenerAdapter() {
             interaction.replyEmbeds(
                 EmbedBuilder()
                     .setTitle("Invite me to your server!")
-                    .setDescription("[Click here](https://discord.com/oauth2/authorize?client_id=963885994468335736&scope=applications.commands&permissions=277025651776)")
+                    .setDescription("[Click here](${api.getInviteUrl(Permission.ADMINISTRATOR)}) to invite me to your server!")
                     .build()
             ).complete()
         }
